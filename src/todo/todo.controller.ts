2063bc49cca72159b0b1f3c86f759fb82ec26668
stephane boguhe <boguhestephane97@gmail.com>
2022-05-17 19:44:45 +0100

    architecture CRUD

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Patch, Delete } from '@nestjs/common';
 import { TodoService } from './todo.service';
 
 @Controller('todo')
@@ -6,7 +6,27 @@ export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
-  getTodos(): string {
-    return this.todoService.sayHello();
+  getAllTodos(): string {
+    return this.todoService.getAllTodos();
+  }
+
+  @Get(':id')
+  getTodo(@Param() params): string {
+    return this.todoService.getTodo(params.id);
+  }
+
+  @Post()
+  addTodo(): string{
+    return this.todoService.addTodo();
+  }
+
+  @Patch(':id')
+  updateTodo(@Param() params): string{
+    return this.todoService.updateTodo(params.id);
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param() params): string{
+    return this.todoService.deleteTodo(params.id);
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TodoService {
-  sayHello(): string {
+  getAllTodos(): string {
     return 'Hello from todo';
+  }
+
+  getTodo(id: string): string {
+    return `Hello ${id}`;
+  }
+
+  addTodo(): string {
+    return 'add todo in this methode';
+  }
+
+  updateTodo(id: string): string {
+    return `this methode update this ${id} todo`;
+  }
+
+  deleteTodo(id: string): string{
+    return `delete this ${id}`;
   }
 }

8553fc6f7c07e605d842ed301548646611be66d2
stephane boguhe <boguhestephane97@gmail.com>
2022-05-20 17:52:23 +0100

    add validator to the fields

--- src/todo/dto/add-todo.dto.ts
+++ src/todo/dto/add-todo.dto.ts
@@ -1,4 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class AddTodoDto {
+  @IsString({
+    message: 'Vous dévez saisir une chaine de caractère',
+  })
+  @IsNotEmpty({
+    message: 'Ce champs est obligatoire',
+  })
   name: string;
+
+  @IsString({
+    message: 'La tâche doit avoir une description',
+  })
+  @IsNotEmpty({
+    message: 'Ce champs est obligatoire',
+  })
   description: string;
 }
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,5 +1,24 @@
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+
 export class UpdateTodoDto {
+  @IsString({
+    message: 'Vous dévez saisir une chaine de caractère',
+  })
+  @IsNotEmpty({
+    message: 'Ce champs est obligatoire',
+  })
   name: string;
+
+  @IsString({
+    message: 'Vous dévez saisir une chaine de caractère',
+  })
+  @IsNotEmpty({
+    message: 'Ce champs est obligatoire',
+  })
   description: string;
+
+  @IsIn(['En cours', 'Terminée'], {
+    message: 'le status doit être soit "En cours" ou "Terminée" ',
+  })
   status: string;
 }

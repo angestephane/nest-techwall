e4d5aa42564e8b92ed5e6a7ce73cc8b2c7bc62e1
stephane boguhe <boguhestephane97@gmail.com>
2022-05-26 11:03:55 +0100

    remove .env from git

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+
+{
+  "type": "mysql",
+  "host": process.env.DB_HOST,
+  "port": parseInt(process.env.DB_PORT),
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "entities": [
+    "Todo"
+  ],
+  "synchronize": true
+}
